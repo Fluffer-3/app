ec0ddce2339c3dbb8d3a146d1f7bf86da234e52c
Mateie <mateie@email.com>
2024-06-20 03:46:31 -0700

    Added more types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -25,8 +25,25 @@ declare type SignupErrors = {
 };
 
 declare type AuthContextType = {
-    user?: any | null;
+    user?: User | null;
     isLoggedIn: boolean;
     login: (userData: any) => void;
     logout: () => void;
+};
+
+declare type User = {
+    id: string;
+    username: string;
+    email: string;
+    avatar?: string | null;
+    nickname?: string | null;
+    bio?: string | null;
+    createdAt;
+    createdTimestamp;
+    updatedAt;
+    updatedTimestamp;
+};
+
+declare type UserWithToken = User & {
+    token: string;
 };
