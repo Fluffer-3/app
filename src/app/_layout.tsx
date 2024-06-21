0d5bc8e8e30e72465f5a10dc419b591bf9bfa943
Mate Machitidze <matemachitidze2001@gmail.com>
2024-06-21 09:51:31 -0700

    Removed server pulse checking for now

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,5 @@
-import "@/global.css";
-
-import { useEffect, useState } from "react";
-import { useQuery } from "@apollo/client";
-
 import { Stack } from "expo-router";
 import ThemeToggle from "@/components/ThemeToggle";
-import { PulseCheck } from "@/gql/general";
 
 export {
     // Catch any errors thrown by the Layout component.
@@ -13,27 +7,15 @@ export {
 } from "expo-router";
 
 export default function RootLayout() {
-    const [pulse, setPulse] = useState(false);
-
-    const { data: { pulse: pulseData } = {} } = useQuery(PulseCheck, {
-        pollInterval: pulse ? 0 : 1000,
-        fetchPolicy: "no-cache"
-    });
-
-    useEffect(() => {
-        if (pulseData) setPulse(true);
-    }, [pulseData]);
-
     return (
         <Stack
-            initialRouteName={pulse ? "log-in" : "server-offline"}
+            initialRouteName="log-in"
             screenOptions={{
                 headerBackVisible: false,
                 title: "",
                 headerRight: () => <ThemeToggle />
             }}
         >
-            <Stack.Screen name="server-offline" />
             <Stack.Screen name="log-in" />
             <Stack.Screen name="sign-up" />
             <Stack.Screen name="+not-found" />
