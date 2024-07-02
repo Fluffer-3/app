781bdc60465b8f36f57c733f10b9d83f7b051346
Mate Machitidze <matemachitidze2001@gmail.com>
2024-07-01 20:51:12 -0700

    removed yes

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Tabs } from "expo-router";
 
 import { useAuth } from "@/providers/AuthProvider";
-import { AppModeProvider } from "@/providers/AppModeProvider";
 
 export default function AuthGuard() {
     const { isLoggedIn } = useAuth();
